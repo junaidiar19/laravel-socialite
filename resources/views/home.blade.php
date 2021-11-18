ba9c51ad6e5e200c07042f7ac21e83496b0faf41
Junaidi Abdul Rahman <junaidiar.fcb@gmail.com>
2021-11-18 15:05:26 +0800

    Update Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,7 +28,7 @@
             <div class="card">
                 <div class="card-header"></div>
                 <div class="card-body">
-
+                    
                 </div>
             </div>
 
